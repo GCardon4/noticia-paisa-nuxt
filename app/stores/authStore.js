5ff8dc23141c1c90e18eafc31d5d86843a85abfc
Gustavo Cardona <admin@gustavocardona.com>
2026-03-10 20:54:02 -0700

    Script sin dependencia

--- app/stores/authStore.js
+++ app/stores/authStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
 export const useAuthStore = defineStore('auth', () => {
   // useSupabaseClient y useSupabaseUser son auto-importados por @nuxtjs/supabase
